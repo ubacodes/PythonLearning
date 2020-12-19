e0c042c89349a7b2ff164d9626a5b1a344411d53
UmutBatuhanALPAN <64593480+ubacodes@users.noreply.github.com>
2020-12-19 20:26:36 +0300

    Update list.py

--- list.py
+++ list.py
@@ -1,6 +1,6 @@
-yliste=[15,25,100,64,75,99,56,"batuhan","umut","buglem"]
+yliste=[15,25,100,64,75,99,56,"ahmet","mehmet","hamdi"]
 
 for a in yliste:
-    if a=="batuhan" :
+    if a=="hamdi" :
         print(len(yliste),a,len(a))
         
